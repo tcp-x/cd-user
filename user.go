31a3ed63f68f7179ae77e162ccbf5936100abe98
georemo <george.oremo@gmail.com>
2024-02-26 20:55:37 +0300

    set version v0.0.2

--- user.go
+++ user.go
@@ -8,7 +8,19 @@ func Login(data string) string {
 	return resp
 }
 
-func ModInfo(data string) string {
+/*
+- Consider auth using public-key signed by corpdesk CA
+*/
+func Auth(data string) string {
+	fmt.Println("User::Auth()/input data:", data)
+	resp := "{name:User, version:0.0.7 publisher: \"EMP Services Ltd\"}"
+	return resp
+}
+
+/*
+- Present varifiable email address
+*/
+func Register(data string) string {
 	fmt.Println("User::Auth()/input data:", data)
 	resp := "{name:User, version:0.0.7 publisher: \"EMP Services Ltd\"}"
 	return resp
