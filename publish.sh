e42db9c3e9efd46b9f6b668f037de1adf8082588
georemo <george.oremo@gmail.com>
2024-03-04 10:57:37 +0300

    set version v0.0.13

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ go build -buildmode=plugin -o ../plugins/Session.so session.go
 cd ..
 
 # set latest version
-Version="v0.0.12"
+Version="v0.0.13"
 go mod tidy
 git submodule update --remote
 git add go.mod user/user.go plugin/User.so session/session.go plugin/Session.so
